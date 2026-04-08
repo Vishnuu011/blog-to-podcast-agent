624073d8431f48fdf3a23f83f2fd922724b5d5f2
Vishnuu011 <vishnurrajeev@gmail.com>
2026-04-08 13:10:45 +0530

    setup.update

---
+++ src/config/config.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+from pathlib import Path
+from typing import Optional, Tuple
+
+
+
+load_dotenv(
+    Path(__file__).parent.parent.parent / ".env"
+)
+
+
+class Settings(BaseSettings):
+
+    GROQ_API_KEY: str 
+    FIRECRAWL_API_KEY: str
+    ELEVENLABS_KEY: str
+    SECRET_KEY: str 
+    ALGORITHM: str 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    class config:
+
+        env_file=".env"
+
+
+settings = Settings()                
+
---
+++ src/controllers/agentController.py
@@ -0,0 +1,16 @@
+import os
+from uuid import uuid4
+from agno.agent import Agent
+from agno.run.agent import RunOutput
+from agno.models.groq import Groq
+from agno.tools.firecrawl import FirecrawlTools
+from elevenlabs import ElevenLabs
+
+from src.logger.custom_logger import logger
+
+
+
+
+
+
+
